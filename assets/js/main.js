442dfced83d71240163982a8db65cd0b08e34f70
Habon Beegsi <habonb@Habons-MacBook-Air.local>
2024-02-12 12:31:22 +0100

    first commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,6 +19,7 @@ if(navClose){
     })
 }
 
+
 /*=============== REMOVE MENU MOBILE ===============*/
 const navLink = document.querySelectorAll('.nav__link')
 
@@ -34,7 +35,33 @@ const sr = ScrollReveal({
     distance: '90px',
     duration: 3000,
 })
-
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        // default items count
+        items: 3,
+        // autoplay is optional
+        // autoplay: false,
+        margin: 30,
+        loop: true,
+        dots: true,
+        // responsive settings
+        responsive: {
+          0: {
+              items: 1 // In extra-small devices (mobile phones), show 1 item
+          },
+          576: {
+              items: 2 // In small devices (larger phones), show 2 items
+          },
+          768: {
+              items: 2 // In medium devices (tablets), show 2 items
+          },
+          992: {
+              items: 3 // In large devices (desktops), show 3 items
+          }
+        }
+    });
+  });
+  
 sr.reveal(`.home__data`, {origin: 'top', delay: 400})
 sr.reveal(`.home__img`, {origin: 'bottom', delay: 600})
 sr.reveal(`.home__footer`, {origin: 'bottom', delay: 800})
